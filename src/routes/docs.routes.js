da4d9540d24c1afa47cc7320f7dc174a3c570889
Josue Morales <thejosuescript@gmail.com>
2023-12-21 18:02:39 -0600

    middleware insert val docs [fix&feature]

--- src/routes/docs.routes.js
+++ src/routes/docs.routes.js
@@ -1,9 +1,10 @@
 const { Router } = require('express')
 const {createAndAddDoc, verifyDocsObligatorios, verifyVigenciaDocs, getDocsOfInsumo} = require('../controllers/docs.controllers')
+const insercionValidator = require('../middlewares/insercionValidator')
 
 const docsRouter = Router()
 
-docsRouter.post('/api/v1/insumo/docs',createAndAddDoc)
+docsRouter.post('/api/v1/insumo/docs',insercionValidator,createAndAddDoc)
 docsRouter.get('/api/v1/insumo/docs',getDocsOfInsumo)
 docsRouter.get('/api/v1/insumo/docs/obligatorios',verifyDocsObligatorios)
 docsRouter.get('/api/v1/insumo/docs/vigencia',verifyVigenciaDocs)
