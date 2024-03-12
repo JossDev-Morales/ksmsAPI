72ad9e3c2ea6a022764a9159b67b77d7dea8a226
Joss morales <thejosuescript@gmail.com>
2024-03-12 11:16:50 -0700

    get webhooks

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -1,11 +1,24 @@
 const { Router } = require("express");
 const responses = require("../models/hooksResponses");
+const { v4 } = require("uuid");
 
 const webhooks=Router()
 webhooks.post("/webhook/response",async (req,res)=>{
     try {
         const response=req.body
-        await responses.create({response})
+        const id=v4()
+        await responses.create({idResponse:id,response})
+        console.log({id,timestmap:new Date()});
+        res.sendStatus(200)
+    } catch (error) {
+        throw error
+    }
+})
+webhooks.get("webhook/response",async (req,res)=>{
+    try {
+        const {id}=req.body
+        const response=await responses.findByPk(id)
+        res.status(200).json(response.response)
     } catch (error) {
         throw error
     }
